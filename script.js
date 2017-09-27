7708182d449f8cd89eba89cd5fc34cd84b7cf13a
Curtis Lewis <lewis.curtis.t@gmail.com>
2017-09-27 14:33:46 -0600

    fixes

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // script.js
 
 /* Variable Declarations */
-var buttonD3 = document.getElementById("D3_button");
-var buttonD6 = document.getElementById("D6_button");
+var button = document.getElementById("button");
 
 /* Function Declarations */
 function rollD3() {
-	var result = 0;
+	var result = 3;
 
 	document.getElementById("D3result").innerHTML = result;
 }
 
 /* Event Listeners */
-buttonD3.addEventListener("click", rollD3);
+button.addEventListener("click", rollD3);
