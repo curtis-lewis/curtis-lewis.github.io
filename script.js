2cc204ab8911f0765159beb7a91d4d43f14bc06a
Curtis Lewis <lewis.curtis.t@gmail.com>
2017-10-02 19:41:39 -0600

    movie/dinner recommendation changes

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // script.js
 
 /* Variable Declarations */
-var button = document.getElementById("button");
-var text = "sample";
+
+// dinner variables
+var buttonDinner = document.getElementById("button_dinner");
+var paragraphDinner = document.getElementById("paragraph_dinner");
+var dinnerCuisines = ["Italian", "Mexican", "Chinese", "Japanese", "Mongolian", "Indian", "American", "Greek", "Fast food"];
+
+// movie variables
+var buttonMovie = document.getElementById("button_movie");
+var paragraphMovie = document.getElementById("paragraph_movie");
+var movieGenres = ["Comedy", "Horror", "Drama", "Romance", "Documentary", "Family", "Feel-good", "Inspirational", "Action", "Sci-Fi", "Fantasy"];
 
 /* Function Declarations */
-function test() {
-	document.getElementById("paragraph").innerHTML = text;
+function getDinner() {
+    paragraphDinner.innerHTML = dinnerCuisines[Math.floor(Math.random() * dinnerCuisines.length)];
+}
+
+function getMovie() {
+    paragraphMovie.innerHTML =movieGenres[Math.floor(Math.random() * movieGenres.length)];
 }
 
 /* Event Listeners */
-button.addEventListener("click", test);
+buttonDinner.addEventListener("click", getDinner);
+buttonMovie.addEventListener("click", getMovie);
