82bd5028b15a821161ca54c670ed0b46ac0ee37d
Curtis Lewis <lewis.curtis.t@gmail.com>
2017-10-18 14:50:24 -0600

    added picture change

--- script.js
+++ script.js
@@ -41,3 +41,10 @@
 //    getDinner();
 //    getMovie();
 //});
+
+var buttonRefresh = document.getElementById("button_refresh");
+
+function refresh() {
+    document.getElementById('image_dinner_top').src = 'chinese_food.jpeg';
+    console.log("button pressed");
+}
