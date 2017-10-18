6f21436bb02fafd059adcc3a9356c61a3c86e2a5
Curtis Lewis <lewis.curtis.t@gmail.com>
2017-10-18 14:53:26 -0600

    removed some comments

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
 // script.js
-//
-///* Variable Declarations */
-//var count = 0;
-//
-//// dinner variables
-//var buttonRandom = document.getElementById("button_random");
-//var paragraphDinner = document.getElementById("paragraph_dinner");
-//var dinnerCuisines = ["Italian", "Mexican", "Chinese", "Japanese", "Mongolian", "Indian", "American", "Greek", "Fast food"];
-//
-//// movie variables
-//var paragraphMovie = document.getElementById("paragraph_movie");
-//var movieGenres = ["Comedy", "Horror", "Drama", "Romance", "Documentary", "Family", "Feel-good", "Inspirational", "Action", "Sci-Fi", "Fantasy", "Western", "Superhero", "Timepiece"];
-//
-///* Function Declarations */
-//function getDinner() {
-//    var dinnerResult = dinnerCuisines[Math.floor(Math.random() * dinnerCuisines.length)];
-//    paragraphDinner.innerHTML = dinnerResult;
-//}
-//
-//function getMovie() {
-//    var movieResult =  movieGenres[Math.floor(Math.random() * movieGenres.length)];
-//    paragraphMovie.innerHTML = movieResult;
-//    if (movieResult == "Comedy") {
-//        paragraphMovie.style.fontFamily="cursive";
-//        console.log("it worked");
-//    }
-//}
-//
-//function resetFont() {
-//    paragraphDinner.style.fontFamily="Encode Sans Expanded, sans-serif";
-//    paragraphMovie.style.fontFamily="Encode Sans Expanded, sans-serif";   
-//}
-//
-///* Event Listeners */
-//buttonRandom.addEventListener("click", function () {
-//    if (count > 0) {
-//        resetFont();
-//    }
-//    count++;
-//    getDinner();
-//    getMovie();
-//});
 
 var buttonRefresh = document.getElementById("button_refresh");
 
